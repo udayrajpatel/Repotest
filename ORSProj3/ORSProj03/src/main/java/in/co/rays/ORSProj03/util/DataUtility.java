038d48c94dcb396f9298bc59a8b921b5c8d36793
udayrajpatel <uday8027@gmail.com>
2020-12-07 18:29:07 +0530

    test1

---
+++ ORSProj3/ORSProj03/src/main/java/in/co/rays/ORSProj03/util/DataUtility.java
@@ -0,0 +1,14 @@
+package in.co.rays.ORSProj03.util;
+/**
+ * Data utility class to formate data one form to another form
+ * @author uday
+ *
+ */
+public class DataUtility {
+
+	/**
+	 * Apllication Date formate class
+	 */
+	
+	
+}
